4e7fd2988d239fda81c12d47397ac010af8040fe
jorgitotorres <56372980+jorgitotorres@users.noreply.github.com>
2020-01-28 23:39:58 -0500

    update before bed

--- config.py
+++ config.py
@@ -1,1 +1,4 @@
 ykey = 'tNVSPxTZuK97fliYWToZrZpCGASCD0HlyF7s554dF_IdGdO1vAenbda05dWc6zUrsvXPE25NpvVJw1ZhohHYp-4n68rsIzrDPQAwh0lceNXGdjprDYMTAvHM_oMvXnYx' 
+api_url = 'https://api.yelp.com'
+search_path = '/v3/businesses/search'
+business_path = '/v3/businesses/'
